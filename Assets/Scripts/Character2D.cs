d832f09bb1d4bcc5831bd6fc02f07b2cef7b80ce
denialfaro <denilorenaalfaro@hotmail.com>
2021-01-27 23:09:14 -0700

    salto con monedas

--- Assets/Scripts/Character2D.cs
+++ Assets/Scripts/Character2D.cs
@@ -2,33 +2,75 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class Character2D : MonoBehaviour
 {
-    Animator anim;
+    [SerializeField, Range(0.1f, 10f)]
+    float moveSpeed = 1f;
+
     SpriteRenderer spr;
+    Animator anim;
+    [SerializeField, Range(0.1f, 15f)]
+    float jumpForce;
+    Rigidbody2D rb2D;
 
-    [SerializeField, Range(0.1f, 20f)]
-    float moveSpeed = 2f;
+    [SerializeField]
+    Color rayColor = Color.magenta;
+    [SerializeField, Range(0.1f, 15f)]
+    float rayDistance = 5f;
+    [SerializeField]
+    LayerMask groundLayer;
 
-    void Awake() {
-        anim = GetComponent<Animator>();
+    void Awake()
+    {
         spr = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
+        rb2D = GetComponent<Rigidbody2D>();
     }
 
-    void Update(){
-        transform.Translate(Vector2.right * Axis.x * moveSpeed * Time.deltaTime);
-        spr.flipX = Flip;
+    void Update()
+    {
+        transform.Translate(Vector2.right * axis.x * moveSpeed * Time.deltaTime);
+        if(jumpButton && IsGrounding)
+        {
+            anim.SetTrigger("jump");
+            rb2D.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        }
     }
 
-    void LateUpdate() {
-        anim.SetFloat("moveX", Mathf.Abs(Axis.x));
+    void LateUpdate() 
+    {
+        spr.flipX = flipSprite;
+        anim.SetFloat("moveX", Mathf.Abs(axis.x));
     }
 
-    Vector2 Axis {
-        get => new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+    //para cosas de fisica
+    void FixedUpdate() 
+    {
+        //Debug.Log(rb2D.velocity.normalized.y);
+        anim.SetFloat("velocityY", rb2D.velocity.normalized.y);
+        anim.SetBool("ground", IsGrounding);
     }
 
-    bool Flip {
-        get => Axis.x > 0f ? false : Axis.x < 0f ? true : spr.flipX;
+    Vector2 axis => new Vector2(Input.GetAxis("Horizontal"),  Input.GetAxis("Vertical"));
+
+    bool flipSprite => axis.x > 0 ? false : axis.x < 0 ? true : spr.flipX;
+
+    bool jumpButton => Input.GetButtonDown("Jump");
+
+    bool IsGrounding => Physics2D.Raycast(transform.position, Vector2.down, rayDistance, groundLayer); 
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = rayColor;
+        Gizmos.DrawRay(transform.position, Vector2.down * rayDistance);    
+    }
+
+    void OnTriggerEnter2D(Collider2D other) {
+        if(other.CompareTag("Coin")) {
+            Coin coin = other.GetComponent<Coin>();
+            Destroy(other.gameObject);
+            Debug.Log(coin.Points);
+        }
     }
 }
+
